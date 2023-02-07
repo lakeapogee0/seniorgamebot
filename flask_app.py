2b125713795ec477877a768167d8eb44ee99fe51
lake <lake_apogee.0q@icloud.com>
2023-02-07 04:17:19 +0000

    all my testing so far

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,30 @@
-from flask import Flask, request, Response
+import os
+import json
+
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+id = "abcdefg"
+print(id)
+from flask import Flask, request
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+
 
 @app.route('/foo', methods=["POST"])
 def foo_page():
-    req_data = request.get_json()
-    message = req_data['message']
-    print(message)
-    return Response(status=200)
+    data = request.get_json()
+    msg = data
+    print(msg)
+    send_message(msg)
+    return "ok", 200
+
+def send_message(msg):
+  url  = 'https://api.groupme.com/v3/bots/post'
+
+  data = {
+          'bot_id' : os.getenv('GROUPME_BOT_ID'),
+          'text'   : msg,
+         }
+  request = Request(url, urlencode(data).encode())
+  #json = urlopen(request).read().decode()
