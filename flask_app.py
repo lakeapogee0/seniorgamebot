7b07f7dfdb9e2b0bffb2b45beff440f6a2e6a2e8
lakeapogee0 <lake_apogee.0q@icloud.com>
2023-02-08 03:45:57 +0000

    got . working

--- flask_app.py
+++ flask_app.py
@@ -3,9 +3,15 @@ print(id)
 from flask import Flask, request
 import requests
 import json
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+botId = os.getenv('BOT_ID')
+print(botId)
+
+
 
 
 app = Flask(__name__)
@@ -19,18 +25,20 @@ app = Flask(__name__)
 @app.route('/foo', methods=['POST'])
 def foo():
     request_data = request.get_json()
-
+    load_dotenv()
+    botId =  os.getenv('BOT_ID')
+    print(botId)
     if request_data["name"] == "test":
         pass
     elif "❌💀" in request_data["text"]:
         r = requests.post("https://api.groupme.com/v3/bots/post", json ={
-          "bot_id"  : "81110e5203f6018341de37d901",
+          "bot_id"  : botId,
           "text"    : request_data["name"] + " was assassinated\nRIP 💀"
         })
     else:
         r = requests.post("https://api.groupme.com/v3/bots/post", json ={
-          "bot_id"  : "81110e5203f6018341de37d901",
-            "text"    : request_data["text"]
+          "bot_id"  : botId,
+          "text"    : request_data["text"]
         })
 
         print(r.text)
